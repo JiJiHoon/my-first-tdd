0a7165dbe2b3d080789432530564a3c55f8b1590
Jihoon Park <chad9349@gmail.com>
2022-06-03 18:09:48 +0900

    add Dollar class

---
+++ src/java/main/study/Dollar.java
@@ -0,0 +1,13 @@
+package study;
+
+public class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier) {
+        this.amount *= multiplier;
+    }
+}
