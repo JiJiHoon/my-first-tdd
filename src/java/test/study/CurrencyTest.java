698de1938a8b236ec3402fb2918205ac80cdb5e8
Jihoon Park <chad9349@gmail.com>
2022-06-03 18:19:15 +0900

    modify multiplication test

--- src/java/test/study/CurrencyTest.java
+++ src/java/test/study/CurrencyTest.java
@@ -2,7 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CurrencyTest {
     @Test
@@ -11,9 +11,15 @@ public class CurrencyTest {
         Dollar five = new Dollar(5);
 
         // when
-        five.times(2);
+        Dollar product = five.times(2);
 
         // then
-        assertEquals(10, five.amount);
+        assertEquals(10, product.amount);
+
+        // when
+        product = five.times(3);
+
+        // then
+        assertEquals(15, product.amount);
     }
 }
