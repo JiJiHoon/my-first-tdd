79e4a691bc7d4660e96625a01f8c3d58e63ad1a1
Jihoon Park <chad9349@gmail.com>
2022-06-04 11:35:07 +0900

    remove duplicated codes

--- src/java/test/study/CurrencyTest.java
+++ src/java/test/study/CurrencyTest.java
@@ -19,31 +19,12 @@ public class CurrencyTest {
     void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(10));
-
-        assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(10));
-
         assertNotEquals(Money.franc(5), Money.dollar(5));
-    }
-
-    @Test
-    void testFrancMultiplication() {
-        // given
-        Franc five = Money.franc(5);
-
-        // then
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    void testDifferentClassEquality() {
-        assertEquals(new Money(10, "CHF"), new Franc(10, "CHF"));
     }
 }
