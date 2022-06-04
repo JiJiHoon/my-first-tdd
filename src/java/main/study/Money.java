1da96f8dd5be017283c03a8329ebf3cef3ffbacc
Jihoon Park <chad9349@gmail.com>
2022-06-04 11:06:47 +0900

    add static factory method

--- src/java/main/study/Money.java
+++ src/java/main/study/Money.java
@@ -1,7 +1,13 @@
 package study;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
--- src/java/test/study/CurrencyTest.java
+++ src/java/test/study/CurrencyTest.java
@@ -8,17 +8,17 @@ public class CurrencyTest {
     @Test
     void testMultiplication() {
         // given
-        Dollar five = new Dollar(5);
+        Money five = Money.dollar(5);
 
         // then
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(10)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(10)));
 
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(10)));
