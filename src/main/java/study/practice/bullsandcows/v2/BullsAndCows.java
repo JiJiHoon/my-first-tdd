4c2307d9e9c7c4b602007167c07ec71c31aabc3a
Jihoon Park <chad9349@gmail.com>
2022-06-09 20:57:52 +0900

    feat: add `getMessage` method

---
+++ src/main/java/study/practice/bullsandcows/v2/BullsAndCows.java
@@ -0,0 +1,8 @@
+package study.practice.bullsandcows.v2;
+
+public class BullsAndCows {
+    private static final String NEW_LINE = System.lineSeparator();
+    public String getMessage() {
+        return "1: single play mode" + NEW_LINE + "2: quit" + NEW_LINE + "Select Mode: ";
+    }
+}
--- src/test/java/study/practice/bullsandcows/v2/BullsAndCowsTest.java
+++ src/test/java/study/practice/bullsandcows/v2/BullsAndCowsTest.java
@@ -2,14 +2,20 @@ package study.practice.bullsandcows.v2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class BullsAndCowsTest {
+    private static final String NEW_LINE = System.lineSeparator();
+
     @Test
-    void asdf() {
+    void startMessage() {
         // given
+        BullsAndCows bullsAndCows = new BullsAndCows();
 
         // when
+        String actual = bullsAndCows.getMessage();
 
         // then
-
+        assertThat(actual).isEqualTo("1: single play mode" + NEW_LINE + "2: quit" + NEW_LINE + "Select Mode: ");
     }
 }
